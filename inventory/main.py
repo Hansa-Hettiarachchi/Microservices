ef3b2e5dfe8ad0e84c11ad687f6d4d5759f1e37c
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-05-03 13:04:34 +0530

    creation of app

---
+++ inventory/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from redis_om import get_redis_connection
+
+app = FastAPI()
+redis = get_redis_connection(
+    host="redis-14179.c258.us-east-1-4.ec2.cloud.redislabs.com",
+    port=14179,
+    password="hqCNKNsQT9InaSk1OMh4iWRmBHk5Ufom",
+    decode_responses=True,
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
