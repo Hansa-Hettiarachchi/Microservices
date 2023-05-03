2dafe6ac25c5e5d9c02f331552aea76c6a6ab53e
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-05-03 15:47:34 +0530

    product service

---
+++ payment/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+import requests
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:8000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+redis = get_redis_connection(
+    host="redis-14179.c258.us-east-1-4.ec2.cloud.redislabs.com",
+    port=14179,
+    password="hqCNKNsQT9InaSk1OMh4iWRmBHk5Ufom",
+    decode_responses=True,
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee : float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
+
+
+@app.post("/orders")
+async def create(request: Request):
+    body = await request.json()
+
+    req = requests.get(f"http://localhost:8000/products/%s" %body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=product['price'] * 0.2,
+        total=product['price'] * 1.2,
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    order_completed(order)
+    return order
+
+
+def order_completed(order: Order):
+    order.status = 'completed'
+    order.save()
